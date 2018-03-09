2114664d99b78a1102dbc96dd309746e5b8f2224
WebSter <wws.dev@gmail.com>
2018-03-10 00:43:20 +0300

    Separated usecases for roles and added usecases for lease expiry business-process

--- UML/generateUML.bat
+++ UML/generateUML.bat
@@ -1,2 +1,4 @@
-java -jar plantuml.jar -charset UTF-8 usecaseDiag.txt
+java -jar plantuml.jar -charset UTF-8 usecasesManager.txt
+java -jar plantuml.jar -charset UTF-8 usecasesClient.txt
+java -jar plantuml.jar -charset UTF-8 usecasesOperator.txt
 java -jar plantuml.jar -charset UTF-8 classDiag.txt
