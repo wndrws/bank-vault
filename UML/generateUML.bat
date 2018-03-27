caf650151d86ecc449b12a8899644812665f15e7
WebSter <wws.dev@gmail.com>
2018-03-28 00:51:09 +0300

    Sequence diagram modification

--- UML/generateUML.bat
+++ UML/generateUML.bat
@@ -2,3 +2,4 @@ java -jar plantuml.jar -charset UTF-8 usecasesManager.txt
 java -jar plantuml.jar -charset UTF-8 usecasesClient.txt
 java -jar plantuml.jar -charset UTF-8 usecasesOperator.txt
 java -jar plantuml.jar -charset UTF-8 classDiag.txt
+java -jar plantuml.jar -charset UTF-8 sequenceDiag.txt
