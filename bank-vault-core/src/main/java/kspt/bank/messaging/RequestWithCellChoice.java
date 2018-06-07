cc44424d22d2e70f166d70587f85439d2b3d0b49
WebSter <wws.dev@gmail.com>
2018-06-07 05:27:10 +0300

    Removed obsolete and unused classes.

--- bank-vault-core/src/main/java/kspt/bank/messaging/RequestWithCellChoice.java
+++
@@ -1,9 +0,0 @@
-package kspt.bank.messaging;
-
-import kspt.bank.enums.CellSize;
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
-public class RequestWithCellChoice extends Request {
-    public final CellSize cellSize;
-}
--- bank-vault-core/src/main/java/kspt/bank/messaging/RequestWithClientInfo.java
+++
@@ -1,9 +0,0 @@
-package kspt.bank.messaging;
-
-import kspt.bank.domain.entities.PassportInfo;
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
-public class RequestWithClientInfo extends Request {
-    public final PassportInfo passportInfo;
-}
