cb90e8349c2465f0c62412d02a0d0dd28d521cd1
WebSter <wws.dev@gmail.com>
2018-12-10 02:12:59 +0300

    Configured embedded web server to serve static resources from filesystem

--- bank-vault-core/src/main/java/kspt/bank/config/WebConfig.java
+++ bank-vault-core/src/main/java/kspt/bank/config/WebConfig.java
@@ -2,6 +2,7 @@ package kspt.bank.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -9,5 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("http://localhost:53322").allowedMethods("*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("file:/public/www/", "classpath:/META-INF/resources/");
     }
 }
